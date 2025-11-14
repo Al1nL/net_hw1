b943a7b823f553fa9c4212bb43bd0924aa802b1c
liorpernik <liorpernik@mail.tau.ac.il>
2025-11-14 12:33:14 +0200

    need to fix main

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import helper
+import sys
 
 HOST = "localhost"
 PORT = 1337
@@ -7,9 +8,17 @@ def main():
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSock:
             clientSock.connect((HOST, PORT))
-            welcome = helper.recvall(clientSock)
-            print(helper.binary_to_string(welcome))
-
+            req ="welcome"
+            answer = helper.recvall(clientSock)
+            print(helper.binary_to_string(answer))
+            while req!="Password: simplepass\n":
+                print("enter req")
+                req =  sys.stdin.readline()
+                print(req)
+                helper.sendall(clientSock,helper.string_to_binary(req))
+                print("sent")
+            answer = helper.recvall(clientSock)
+            print(helper.binary_to_string(answer))
     except socket.error as msg:
         print(msg)
 
